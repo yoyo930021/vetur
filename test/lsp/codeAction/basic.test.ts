ca54c7c6b48b1a6155201f290c5c8adab66653ab
yoyo930021 <yoyo930021@gmail.com>
2020-03-17 12:09:28 +0800

    Retry to get diagnostics until length > 0 or timeout

--- test/lsp/codeAction/basic.test.ts
+++ test/lsp/codeAction/basic.test.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as assert from 'assert';
 import { activateLS, sleep, showFile, FILE_LOAD_SLEEP_TIME } from '../helper';
 import { getDocUri, sameLineRange } from '../util';
+import { getDiagnosticsAndTimeout } from '../helper';
 
 describe('Should do codeAction', () => {
   const docUri = getDocUri('client/codeAction/Basic.vue');
@@ -9,10 +10,6 @@ describe('Should do codeAction', () => {
   before('activate', async () => {
     await activateLS();
     await showFile(docUri);
-    await sleep(FILE_LOAD_SLEEP_TIME);
-
-    // More sleep for waiting diagnostics
-    await sleep(FILE_LOAD_SLEEP_TIME);
     await sleep(FILE_LOAD_SLEEP_TIME);
   });
 
@@ -34,8 +31,7 @@ interface CodeAction {
 }
 
 async function testCodeAction(docUri: vscode.Uri, range: vscode.Range, expectedActions: CodeAction[]) {
-  // For diagnostics to show up
-  await sleep(2000);
+  await getDiagnosticsAndTimeout(docUri);
 
   const result = (await vscode.commands.executeCommand(
     'vscode.executeCodeActionProvider',
